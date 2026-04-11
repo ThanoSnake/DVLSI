fea168eaa33be709c4ee94139f04b7590eed992f
ThanoSnake <feidakisthanos@gmail.com>
2026-04-11 15:14:53 +0300

    Create dvlsi_lab6_top.vhd

---
+++ Lab4/part2/dvlsi_lab6_top.vhd
@@ -0,0 +1,181 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity dvlsi_lab6_top is
+  port (
+        DDR_cas_n         : inout STD_LOGIC;
+        DDR_cke           : inout STD_LOGIC;
+        DDR_ck_n          : inout STD_LOGIC;
+        DDR_ck_p          : inout STD_LOGIC;
+        DDR_cs_n          : inout STD_LOGIC;
+        DDR_reset_n       : inout STD_LOGIC;
+        DDR_odt           : inout STD_LOGIC;
+        DDR_ras_n         : inout STD_LOGIC;
+        DDR_we_n          : inout STD_LOGIC;
+        DDR_ba            : inout STD_LOGIC_VECTOR( 2 downto 0);
+        DDR_addr          : inout STD_LOGIC_VECTOR(14 downto 0);
+        DDR_dm            : inout STD_LOGIC_VECTOR( 3 downto 0);
+        DDR_dq            : inout STD_LOGIC_VECTOR(31 downto 0);
+        DDR_dqs_n         : inout STD_LOGIC_VECTOR( 3 downto 0);
+        DDR_dqs_p         : inout STD_LOGIC_VECTOR( 3 downto 0);
+        FIXED_IO_mio      : inout STD_LOGIC_VECTOR(53 downto 0);
+        FIXED_IO_ddr_vrn  : inout STD_LOGIC;
+        FIXED_IO_ddr_vrp  : inout STD_LOGIC;
+        FIXED_IO_ps_srstb : inout STD_LOGIC;
+        FIXED_IO_ps_clk   : inout STD_LOGIC;
+        FIXED_IO_ps_porb  : inout STD_LOGIC
+       );
+end entity; -- dvlsi_lab6_top
+
+architecture arch of dvlsi_lab6_top is
+
+  component design_1_wrapper is
+    port (
+          DDR_cas_n         : inout STD_LOGIC;
+          DDR_cke           : inout STD_LOGIC;
+          DDR_ck_n          : inout STD_LOGIC;
+          DDR_ck_p          : inout STD_LOGIC;
+          DDR_cs_n          : inout STD_LOGIC;
+          DDR_reset_n       : inout STD_LOGIC;
+          DDR_odt           : inout STD_LOGIC;
+          DDR_ras_n         : inout STD_LOGIC;
+          DDR_we_n          : inout STD_LOGIC;
+          DDR_ba            : inout STD_LOGIC_VECTOR( 2 downto 0);
+          DDR_addr          : inout STD_LOGIC_VECTOR(14 downto 0);
+          DDR_dm            : inout STD_LOGIC_VECTOR( 3 downto 0);
+          DDR_dq            : inout STD_LOGIC_VECTOR(31 downto 0);
+          DDR_dqs_n         : inout STD_LOGIC_VECTOR( 3 downto 0);
+          DDR_dqs_p         : inout STD_LOGIC_VECTOR( 3 downto 0);
+          FIXED_IO_mio      : inout STD_LOGIC_VECTOR(53 downto 0);
+          FIXED_IO_ddr_vrn  : inout STD_LOGIC;
+          FIXED_IO_ddr_vrp  : inout STD_LOGIC;
+          FIXED_IO_ps_srstb : inout STD_LOGIC;
+          FIXED_IO_ps_clk   : inout STD_LOGIC;
+          FIXED_IO_ps_porb  : inout STD_LOGIC;
+          --------------------------------------------------------------------------
+          ----------------------------------------------- PL (FPGA) COMMON INTERFACE
+          ACLK                                : out STD_LOGIC;
+          ARESETN                             : out STD_LOGIC_VECTOR(0 to 0);
+          ------------------------------------------------------------------------------------
+          -- PS2PL-DMA AXI4-STREAM MASTER INTERFACE TO ACCELERATOR AXI4-STREAM SLAVE INTERFACE
+          M_AXIS_TO_ACCELERATOR_tdata         : out STD_LOGIC_VECTOR(7 downto 0);
+          M_AXIS_TO_ACCELERATOR_tkeep         : out STD_LOGIC_VECTOR( 0    to 0);
+          M_AXIS_TO_ACCELERATOR_tlast         : out STD_LOGIC;
+          M_AXIS_TO_ACCELERATOR_tready        : in  STD_LOGIC;
+          M_AXIS_TO_ACCELERATOR_tvalid        : out STD_LOGIC;
+          ------------------------------------------------------------------------------------
+          -- ACCELERATOR AXI4-STREAM MASTER INTERFACE TO PL2P2-DMA AXI4-STREAM SLAVE INTERFACE
+          S_AXIS_S2MM_FROM_ACCELERATOR_tdata  : in  STD_LOGIC_VECTOR(31 downto 0);
+          S_AXIS_S2MM_FROM_ACCELERATOR_tkeep  : in  STD_LOGIC_VECTOR( 3 downto 0);
+          S_AXIS_S2MM_FROM_ACCELERATOR_tlast  : in  STD_LOGIC;
+          S_AXIS_S2MM_FROM_ACCELERATOR_tready : out STD_LOGIC;
+          S_AXIS_S2MM_FROM_ACCELERATOR_tvalid : in  STD_LOGIC
+         );
+  end component design_1_wrapper;
+
+-------------------------------------------
+-- INTERNAL SIGNAL & COMPONENTS DECLARATION
+
+  -- internal signals
+  signal aclk         : std_logic;
+  signal aresetn      : std_logic_vector(0 to 0);
+  
+  -- Signals from PS to Accelerator
+  signal ps_data     : std_logic_vector(7 downto 0);
+  signal ps_valid    : std_logic;
+  
+  -- Signals from Accelerator to PS
+  signal acc_red      : std_logic_vector(7 downto 0);
+  signal acc_green    : std_logic_vector(7 downto 0);
+  signal acc_blue     : std_logic_vector(7 downto 0);
+  signal acc_valid    : std_logic;
+  signal acc_finished : std_logic;
+  
+  -- Check new_image
+  signal s_new_image  : std_logic := '0';
+  signal s_waiting_for_first_pixel : std_logic := '1';
+
+begin
+
+  PROCESSING_SYSTEM_INSTANCE : design_1_wrapper
+    port map (
+              DDR_cas_n         => DDR_cas_n,
+              DDR_cke           => DDR_cke,
+              DDR_ck_n          => DDR_ck_n,
+              DDR_ck_p          => DDR_ck_p,
+              DDR_cs_n          => DDR_cs_n,
+              DDR_reset_n       => DDR_reset_n,
+              DDR_odt           => DDR_odt,
+              DDR_ras_n         => DDR_ras_n,
+              DDR_we_n          => DDR_we_n,
+              DDR_ba            => DDR_ba,
+              DDR_addr          => DDR_addr,
+              DDR_dm            => DDR_dm,
+              DDR_dq            => DDR_dq,
+              DDR_dqs_n         => DDR_dqs_n,
+              DDR_dqs_p         => DDR_dqs_p,
+              FIXED_IO_mio      => FIXED_IO_mio,
+              FIXED_IO_ddr_vrn  => FIXED_IO_ddr_vrn,
+              FIXED_IO_ddr_vrp  => FIXED_IO_ddr_vrp,
+              FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
+              FIXED_IO_ps_clk   => FIXED_IO_ps_clk,
+              FIXED_IO_ps_porb  => FIXED_IO_ps_porb,
+              --------------------------------------------------------------------------
+              ----------------------------------------------- PL (FPGA) COMMON INTERFACE
+              ACLK                                => aclk,    -- clock to accelerator
+              ARESETN                             => aresetn, -- reset to accelerator, active low
+              ------------------------------------------------------------------------------------
+              -- PS2PL-DMA AXI4-STREAM MASTER INTERFACE TO ACCELERATOR AXI4-STREAM SLAVE INTERFACE
+              M_AXIS_TO_ACCELERATOR_tdata         => ps_data,
+              M_AXIS_TO_ACCELERATOR_tkeep         => open,
+              M_AXIS_TO_ACCELERATOR_tlast         => open,
+              M_AXIS_TO_ACCELERATOR_tready        => '1',
+              M_AXIS_TO_ACCELERATOR_tvalid        => ps_valid,
+              ------------------------------------------------------------------------------------
+              -- ACCELERATOR AXI4-STREAM MASTER INTERFACE TO PL2P2-DMA AXI4-STREAM SLAVE INTERFACE
+              S_AXIS_S2MM_FROM_ACCELERATOR_tdata  => x"00" & acc_red & acc_green & acc_blue,
+              S_AXIS_S2MM_FROM_ACCELERATOR_tkeep  => "1111", --all bytes valid. Read them all
+              S_AXIS_S2MM_FROM_ACCELERATOR_tlast  => acc_finished,
+              S_AXIS_S2MM_FROM_ACCELERATOR_tready => open,
+              S_AXIS_S2MM_FROM_ACCELERATOR_tvalid => acc_valid
+             );
+
+  
+    --Generate new_image signal
+    process(aclk, aresetn)
+    begin
+        if aresetn(0) = '0' then
+            s_waiting_for_first_pixel <= '1';
+        elsif rising_edge(aclk) then
+            if s_new_image = '1' then
+                s_waiting_for_first_pixel <= '0';
+            elsif acc_finished = '1' then
+                s_waiting_for_first_pixel <= '1';
+            end if;
+        end if;
+    end process;
+
+    s_new_image <= ps_valid and s_waiting_for_first_pixel;
+
+----------------------------
+-- COMPONENTS INSTANTIATIONS
+
+DEBAYER_INST : entity work.debayering_filter
+    generic map (
+        N => 32
+    )
+    port map (
+        clk                 => aclk,
+        rst_n               => aresetn(0),
+        new_image           => s_new_image,
+        valid_in            => ps_valid,
+        pixel               => ps_data,
+        valid_out           => acc_valid,
+        image_finished      => acc_finished,
+        red_out             => acc_red,
+        green_out           => acc_green,
+        blue_out            => acc_blue
+    );
+
+end architecture; -- arch
